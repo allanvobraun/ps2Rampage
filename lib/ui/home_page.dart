75efd92e01cbe37e6fc16589b3528b4b6d44eca2
allanbraun <allanvobraun@gmail.com>
2021-11-15 20:10:07 -0300

    botões hover

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ps2_rampage/ui/drop_zone.dart';
+import 'package:ps2_rampage/ui/widgets/hover_button.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key, required this.title}) : super(key: key);
@@ -11,36 +11,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Dropzone()
+          children: <Widget>[
+            const Dropzone(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HoverButton(
+                    child: const Text('Cancelar'),
+                    color: Colors.red,
+                    onClick: () {}
+                ),
+                HoverButton(
+                    child: const Text('Converter'),
+                    color: Colors.green,
+                    onClick: () {}
+                ),
+              ],
+            )
           ],
         ),
       ),
---
+++ lib/ui/widgets/hover_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HoverButton extends StatefulWidget {
+  final VoidCallback onClick;
+  final Widget child;
+  final MaterialColor color;
+
+  const HoverButton({Key? key,
+    required this.onClick,
+    required this.child,
+    required this.color})
+      : super(key: key);
+
+  @override
+  State<HoverButton> createState() => _HoverState();
+}
+
+class _HoverState extends State<HoverButton> {
+
+  bool _hovered = false;
+
+  ButtonStyle get _style {
+    return OutlinedButton.styleFrom(
+        primary: _hovered ? Colors.white : widget.color,
+        side: BorderSide(color: widget.color),
+        backgroundColor: _hovered ? widget.color : null
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (details) {
+        setState(() {
+          _hovered = true;
+        });
+      },
+      onExit: (details) {
+        setState(() {
+          _hovered = false;
+        });
+      },
+      child: OutlinedButton(
+        onPressed: widget.onClick,
+        child: widget.child,
+        style: _style,
+      ),
+    );
+  }
+}
